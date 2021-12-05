65f85fbb5adfab6a5ee1157c1f296df82ba90b60
jamesmcc-design <JAMES.MCC94@HOTMAIL.CO.UK>
2021-12-05 03:05:03 +0100

    display of drop date and files on MP

--- app/src/App.js
+++ app/src/App.js
@@ -1,13 +1,27 @@
+import { publicKey } from '@project-serum/anchor/dist/cjs/utils';
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import twitterLogo from './assets/twitter-logo.svg';
 import Button from "./components/Button"
+import CandyMachine from './CandyMachine';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+
+  const [walletAddress, setWalletAddress] = useState(null)
+
+  const connectWallet = async () => {
+    const { solana } = window
+
+    if (solana) {
+      const response = await solana.connect()
+        console.log("Connected with public key:",response.publicKey.toString());
+        setWalletAddress(publicKey)
+    }
+  }
 
   const checkIfWalletIsConnected = async () => {
     try {
@@ -18,16 +32,16 @@ const App = () => {
           console.log('Phantom wallet found!')
 
           const response = await solana.connect({ onlyIfTrusted: true })
-            .then(({ publicKey }) => console.log(
+            console.log(
               'Connected with Public Key:',
-              publicKey
-            ))
-            .catch(() => console.log('Could not connect!!'))
-
+              response.publicKey.toString()
+            )
+            setWalletAddress(publicKey)      
         }
-      } else {
-        alert('Solana object not found! Get a Phantom Wallet')
-      }
+
+        } else {
+          alert('Solana object not found! Get a Phantom Wallet')
+        }
     } catch (error) {
       console.error(error)
     }
@@ -47,8 +61,13 @@ const App = () => {
         <div className="header-container">
           <p className="header">🍭 Candy Drop</p>
           <p className="sub-text">NFT drop machine with fair mint</p>
-          {Button("Connect to wallet" , null)}
+          {!walletAddress ?
+            Button("Connect to wallet" , connectWallet)
+            :
+            Button("You're Connected", null)
+          }
         </div>
+        {walletAddress && <CandyMachine walletAddress={window.solana} />}
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
           <a
